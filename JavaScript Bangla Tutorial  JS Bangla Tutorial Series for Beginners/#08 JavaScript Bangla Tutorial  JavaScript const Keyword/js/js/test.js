1541fba068aa29d2a97097ba3cb4d7f63cd8ac82
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-09 19:54:05 +0600

    complete: #08 JavaScript Bangla Tutorial  JavaScript const Keyword

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#08 JavaScript Bangla Tutorial  JavaScript const Keyword/js/js/test.js
@@ -0,0 +1,14 @@
+// let a; // declare
+// a = 5;
+
+// a = 7
+
+// const a = 5;
+// a = 7; // reassign does not allowed
+
+/* not allowed 
+const a;
+a= 5; */
+
+// It does not define a constant value. It defines a constant reference to a value.
+const a = [1, 2, 3, 4,];
