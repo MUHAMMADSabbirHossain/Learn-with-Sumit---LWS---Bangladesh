57b063e83bb07d1e2741f0aead9556917f6209d5
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-09 16:43:13 +0600

    complete: #05 JavaScript Bangla Tutorial  JavaScript Comments

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#05 JavaScript Bangla Tutorial  JavaScript Comments/js/js/test.js
@@ -0,0 +1,9 @@
+console.log("hello"); // eta amagder console e kichu dekhai
+
+/* this is a Comment
+this is multiline
+this is goog */
+console.log("hello");
+
+console.log(a + b);
+console.log(hello);
