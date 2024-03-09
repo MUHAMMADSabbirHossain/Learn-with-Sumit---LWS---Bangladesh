a45f54c1b5f48ae12f984fedd7f9f9e14fa0a1e5
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-09 20:10:57 +0600

    complete: #09 JavaScript Bangla Tutorial | JavaScript Operators

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#09 JavaScript Bangla Tutorial  JavaScript Operators/js/js/test.js
@@ -0,0 +1,10 @@
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+console.log(x); // 10
+console.log(y); // 55
+console.log(z); // Hello5
+console.log(typeof x); // number
+console.log(typeof y); // string
+console.log(typeof z); // string
