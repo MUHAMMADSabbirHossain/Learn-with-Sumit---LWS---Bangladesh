440c39662df9aded4742d466d6bff9443cfc4644
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-10 16:16:14 +0600

    complete: #11 JavaScript Bangla Tutorial  JavaScript Data Type

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#11 JavaScript Bangla Tutorial  JavaScript Data Type/js/index.js
@@ -0,0 +1,9 @@
+let x = 5;
+let y = 6;
+let z = 7;
+
+let myObject = { x: 5, y: 6, z: 7 }; // variableType objectName = {property(name:value),...}
+
+console.log(typeof myObject); // object
+console.log(typeof x);
+console.log(myObject instanceof Object); // true
