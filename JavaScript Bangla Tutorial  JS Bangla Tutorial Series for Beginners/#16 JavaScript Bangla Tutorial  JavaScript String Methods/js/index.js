ac040dc1ed9919c5fc221cb25925dadc4a6a76c8
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-11 19:13:32 +0600

    complete: #16 JavaScript Bangla Tutorial | JavaScript String Methods

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#16 JavaScript Bangla Tutorial  JavaScript String Methods/js/index.js
@@ -0,0 +1,26 @@
+/* let x = "John";
+let y = new String("John")
+
+console.log(x === y); // false */
+
+/* const a = {
+    name: 'Bangladesh',
+    districts: 64,
+    sleep: function () {
+
+    }
+};
+
+const s = "Bangladesh";
+
+console.log(s.length); */
+
+// let str = "Apple, Banana, Kiwi";
+// let banana = str.slice(7, 13)
+// console.log(banana); // Banana
+
+// replace
+let str = "I love Sakina & Sakina";
+let zarina = str.replace("Sakina", "Zarina");
+console.log(zarina); // I love Zarina
+console.log(str); // I love Sakina
