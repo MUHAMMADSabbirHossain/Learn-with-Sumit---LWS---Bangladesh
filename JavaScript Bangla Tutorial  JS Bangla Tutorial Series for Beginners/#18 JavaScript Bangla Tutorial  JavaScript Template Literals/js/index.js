64cc41ec88fda1e371e83e1d4d5ab01728a8b585
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-12 09:25:35 +0600

    complete: #18 JavaScript Bangla Tutorial | JavaScript Template Literals

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#18 JavaScript Bangla Tutorial  JavaScript Template Literals/js/index.js
@@ -0,0 +1,13 @@
+// let a = `Bangladesh`;
+
+// let a = `My life
+// my rule`;
+// console.log(a);
+
+let country = 'Bangladesh';
+let a = 17;
+let b = 1;
+
+// let sentence = 'I love ' + country + 'and it is a big country';
+// let sentence = `I love ${country} and it is a big ${country}`;
+let sentence = `I love ${country} and it's population is ${a + b}`;
