1888e5a98801002666b15665e75aa17577a62cf8
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-03-15 10:55:31 +0600

    #23 JavaScript Bangla Tutorial | JavaScript Array Sort

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#23 JavaScript Bangla Tutorial  JavaScript Array Sort/js/index.js
@@ -0,0 +1,9 @@
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function (a, b) { return a - b });
+
+// console.log(points);
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function () { return 0.5 - Math.random() });
+
+console.log(points);
