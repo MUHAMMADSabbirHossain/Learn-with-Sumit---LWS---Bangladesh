91cdd838b5aacf37c4cdc6e155a5d2d439bff63b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-01 20:12:04 +0600

    complete: #79 JavaScript Bangla Tutorial | JavaScript Class Inheritance

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#79 JavaScript Bangla Tutorial  JavaScript Class Inheritance/scirpt.js
@@ -0,0 +1,51 @@
+/* class Car {
+    constructor(brand) {
+        this.carname = brand;
+    }
+    present() {
+        return 'I have a ' + this.carname;
+    }
+}
+
+class Model extends Car {
+    constructor(brand, mod) {
+        super(brand);
+        this.model = mod;
+    }
+    show() {
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang");
+console.log(myCar.show()); */
+
+class Car {
+    constructor(brand) {
+        this.carName = brand
+    }
+    present() {
+        return "I have a " + this.carName;
+    }
+}
+// console.log(Car);
+// console.log(Car.carName);
+
+// const myCar = new Car("Ford");
+// console.log(myCar);
+// console.log(myCar.carName);
+// console.log(myCar.present());
+
+
+// inheritance
+class Model extends Car {
+    constructor(brand, model) {
+        super(brand);
+        this.model = model;
+    }
+    show() {
+        return this.present() + ", it is a " + this.model;
+    }
+}
+const myCar = new Model("Ford", "Mustang");
+console.log(myCar.show());
