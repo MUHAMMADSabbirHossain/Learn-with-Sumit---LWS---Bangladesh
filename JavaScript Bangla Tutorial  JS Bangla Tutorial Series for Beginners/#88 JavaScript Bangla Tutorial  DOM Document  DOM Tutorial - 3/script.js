a09cb4c980eea516c2fb29eeeb2b5b1117905606
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-03 12:11:59 +0600

    complete: #88 JavaScript Bangla Tutorial | DOM Document | DOM Tutorial - 3

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#88 JavaScript Bangla Tutorial  DOM Document  DOM Tutorial - 3/script.js
@@ -0,0 +1,8 @@
+// console.log(document.getElementById("demo"));
+// console.dir(document.getElementById("demo"));
+
+const p = document.getElementById("demo");
+p.innerHTML = "Test 0";
+
+console.log(document);
+console.dir(document);
