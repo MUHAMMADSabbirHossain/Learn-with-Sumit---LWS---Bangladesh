7a355d0db991a2f3dc90da2cdf3fb17e4c684f39
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-06 15:06:08 +0600

    complete: #102 JavaScript Bangla Tutorial  JS Window object  BOM Tutorial 2 #102 JavaScript Bangla Tutorial  JS Window object  BOM Tutorial 2

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#102 JavaScript Bangla Tutorial  JS Window object  BOM Tutorial 2/script.js
@@ -0,0 +1,16 @@
+let myWindow;
+
+// select dom element
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+
+// show window object properties
+width.innerHTML = "Window inner width is : " + window.innerWidth;
+height.innerHTML = "Window inner height is " + window.innerHeight;
+
+function openWindow() {
+    myWindow = window.open("https://google.com", "_self");
+}
+function closeWindow() {
+    myWindow.close();
+}
