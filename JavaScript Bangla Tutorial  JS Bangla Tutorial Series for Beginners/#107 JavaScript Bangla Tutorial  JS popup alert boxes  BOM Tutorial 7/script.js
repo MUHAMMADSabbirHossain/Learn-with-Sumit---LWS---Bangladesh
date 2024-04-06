edd7b2a212111c9b2cee771c4510b4f376853020
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-06 18:04:02 +0600

    complete: #107 JavaScript Bangla Tutorial  JS popup alert boxes  BOM Tutorial 7

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#107 JavaScript Bangla Tutorial  JS popup alert boxes  BOM Tutorial 7/script.js
@@ -0,0 +1,27 @@
+const confirm = document.getElementById("confirm");
+const prompt = document.getElementById("prompt");
+
+function showAlert() {
+    window.alert("Hello world!");
+}
+
+function showConfirm() {
+    let txt;
+    if (window.confirm("Press a button!")) {
+        txt = "You pressed ok!"
+    } else {
+        txt = "You pressed cancel!"
+    }
+    confirm.innerHTML = txt;
+}
+
+function showPrompt() {
+    let person = window.prompt("Please enter you name: ", "Harry potter");
+    let txt;
+    if (person === null || person === "") {
+        txt = "User cancelled the prompt!";
+    } else {
+        txt = "Hello" + person + "! How are you?";
+    }
+    prompt.innerHTML = txt;
+}
