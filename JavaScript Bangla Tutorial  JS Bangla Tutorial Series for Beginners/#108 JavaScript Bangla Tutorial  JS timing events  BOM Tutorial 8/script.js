402b0645334b17b761b5d693bfee994fab93884c
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-07 05:30:09 +0600

    complete: #108 JavaScript Bangla Tutorial | JS timing events | BOM Tutorial 8

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#108 JavaScript Bangla Tutorial  JS timing events  BOM Tutorial 8/script.js
@@ -0,0 +1,24 @@
+const timeout = document.getElementById("timeout");
+const interval = document.getElementById("interval");
+
+let timeoutInstance;
+let intervalInstance;
+function startTimeout() {
+    timeoutInstance = setTimeout(function () {
+        timeout.innerHTML = "3 seconds over!";
+    }, 3000);
+}
+
+function stopTimeout() {
+    clearTimeout(timeoutInstance);
+}
+
+function startInterval() {
+    intervalInstance = setInterval(() => {
+        interval.innerHTML = new Date().toLocaleTimeString();
+    }, 1000);
+}
+
+function stopInterval() {
+    clearInterval(intervalInstance);
+}
