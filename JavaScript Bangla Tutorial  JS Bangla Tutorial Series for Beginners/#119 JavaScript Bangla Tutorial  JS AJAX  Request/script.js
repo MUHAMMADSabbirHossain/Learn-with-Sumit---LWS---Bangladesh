8399e92c36ed52e78354bc63ae2390272d5f54d4
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-08 13:49:07 +0600

    complete: #119 JavaScript Bangla Tutorial | JS AJAX | Request

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#119 JavaScript Bangla Tutorial  JS AJAX  Request/script.js
@@ -0,0 +1,41 @@
+function loadData(callbackF) {
+    // create a new request
+    const xhr = new XMLHttpRequest();
+
+    // what to do when response arrives
+    // xhr.onload = function () {
+    //     const container = document.getElementById("demo");
+    //     demo.innerHTML = this.responseText;
+    // };
+    xhr.onload = function () {
+        // callbackF(this);
+
+        console.log("Resoponse finished");
+        const container = document.getElementById("demo");
+        container.innerHTML = this.responseText;
+    };
+
+    // prepare request - methods: GET, POST, PUT, PATCH, DELETE, OPTIONS
+    // xhr.open("GET", "./data/data.txt", true);
+    xhr.open("GET", "./data/data.txt", false);
+
+    // add a request headere
+    // xhr.setRequestHeader("MY_GF", "JavavScript");
+
+    // send request
+    xhr.send();
+
+    console.log("hello");
+    // abort
+    // xhr.abort();
+}
+
+function myCallback1(xhr) {
+    const container = document.getElementById("demo");
+    container.innerHTML = xhr.responseText;
+}
+
+function myCallback2(xhr) {
+    const container = document.getElementById("demo2");
+    container.innerHTML = xhr.responseText;
+}
