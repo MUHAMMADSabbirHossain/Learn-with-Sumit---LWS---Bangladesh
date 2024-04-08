37d141156bd9e74f7a1104eedb56867ab3625ed0
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-08 23:53:23 +0600

    complete: #130 JavaScript Bangla Tutorial  JS JSON  JSON HTML

---
+++ JavaScript Bangla Tutorial  JS Bangla Tutorial Series for Beginners/#130 JavaScript Bangla Tutorial  JS JSON  JSON HTML/script.js
@@ -0,0 +1,11 @@
+async function change_myselect(sel) {
+    const dbParam = JSON.stringify({ table: sel, limit: 20 });
+    const response = await fetch("https://www.w3schools.com/js/json_demo_html_table.php", {
+        method: "POST",
+        headers: {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        body: "x=" + JSON.stringify(dbParam)
+    });
+
+}
