e15210b1365afd7ce1e298a33dfbd5fce67c1e8b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-10 11:35:13 +0600

    complete: #1 JavaScript Tips and Tricks - JavaScript Job Interview Questions - Bangla

---
+++ JavaScript Tips and Tricks - JavaScript Job Interview Questions/#1 JavaScript Tips and Tricks - JavaScript Job Interview Questions - Bangla/example-2/two.js
@@ -0,0 +1,10 @@
+// Convert any value to boolean
+
+// Using !! in front of any vlaue
+console.log(!!"mashrafi"); // ture
+console.log(!!1); // ture
+console.log(!!0); // false
+console.log(!!undefined); // false
+
+// We can also use Boolean() to achieve same
+console.log(Boolean("mashrafi")); // true
---
+++ JavaScript Tips and Tricks - JavaScript Job Interview Questions/#1 JavaScript Tips and Tricks - JavaScript Job Interview Questions - Bangla/example-3/three.js
@@ -0,0 +1,8 @@
+// Resizing any array
+// Resizing any array
+let animals = ["🐕", "🐒", "🦊", "🐅"];
+
+// We can use array's length property
+animals.length = 3;
+
+console.log(animals); // ["🐕", "🐒", "🦊"]
