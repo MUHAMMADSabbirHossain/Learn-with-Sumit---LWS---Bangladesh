0e8b36134e8bf11cae1750505d261e0501cd2a42
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-11 21:48:16 +0600

    complete: #14 Just in Time - Advanced Concepts  Tailwind CSS Bangla Tutorial

--- Tailwind CSS Bangla Tutorial Series/#09 @variants Directive  Tailwind CSS Bangla Tutorial/tailwind.config.js
+++ Tailwind CSS Bangla Tutorial Series/#09 @variants Directive  Tailwind CSS Bangla Tutorial/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: [
+    "./public/**/*.{html,js}"
+  ],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
