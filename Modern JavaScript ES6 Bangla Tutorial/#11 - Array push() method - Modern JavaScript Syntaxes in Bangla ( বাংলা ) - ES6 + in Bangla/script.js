88ebd811adcf7f4bb2eacd9def9be81ecde8d829
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-22 17:47:55 +0600

    complete: #11 - Array push() method - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla

---
+++ Modern JavaScript ES6 Bangla Tutorial/#11 - Array push() method - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla/script.js
@@ -0,0 +1,11 @@
+// Array.prototype.push()
+var numbers = [1, 2, 3, 4, 5];
+
+// numbers.push(6, 7, 8, 9);
+// numbers.push([6, 7, 8, 9]);
+
+const result = numbers.push(6, 7, 8, 9);
+
+console.log(result);
+
+console.log(numbers);
