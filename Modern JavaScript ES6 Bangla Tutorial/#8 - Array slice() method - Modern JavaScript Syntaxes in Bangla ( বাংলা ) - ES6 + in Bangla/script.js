1dae62139987e76ed377fdb163d6b534d846bd6a
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-22 16:43:10 +0600

    complete: #8 - Array slice() method - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla

---
+++ Modern JavaScript ES6 Bangla Tutorial/#8 - Array slice() method - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla/script.js
@@ -0,0 +1,9 @@
+// Array.prototype.slice()
+var numbers = [1, 2, 3, 4, 5];
+
+// var result = numbers.slice(1, 3); // 2, 3
+// var result = numbers.slice(-2, -1);  // 4
+var result = numbers.slice(-2, 4); // 4
+// var result = numbers.slice(); // [1, 2, 3, 4, 5]
+console.log(numbers);
+console.log(result); // 
