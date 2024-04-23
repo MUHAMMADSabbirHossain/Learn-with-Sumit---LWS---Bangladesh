5f0f509e8a61293cab6b13c7c9dcc47c9b33d48b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-04-23 13:42:29 +0600

    complete: #21 - Template Literals - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla

---
+++ Modern JavaScript ES6 Bangla Tutorial/#21 - Template Literals - Modern JavaScript Syntaxes in Bangla ( বাংলা ) - ES6 + in Bangla/script.js
@@ -0,0 +1,11 @@
+// templete literals
+
+var a = 5;
+var b = 6;
+
+console.log("I am " + a + " and I am " + b);
+console.log(`I am ${a} and I am ${b}`);
+console.log(`I am ${a + b}`);
+console.log(`I am ${a * b}`);
+console.log(`I am test
+        this is stest`);        
