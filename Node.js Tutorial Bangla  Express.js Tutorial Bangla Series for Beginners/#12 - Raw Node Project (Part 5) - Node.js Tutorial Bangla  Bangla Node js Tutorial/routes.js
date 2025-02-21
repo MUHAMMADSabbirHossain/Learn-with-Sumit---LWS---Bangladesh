dbddecaa698b9a4221437b484c8d3ada7eb9be18
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2025-02-21 17:36:52 +0600

    complete: #12 - Raw Node Project (Part 5) - Node.js Tutorial Bangla | Bangla Node js Tutorial

--- Node.js Tutorial Bangla  Express.js Tutorial Bangla Series for Beginners/#12 - Raw Node Project (Part 5) - Node.js Tutorial Bangla  Bangla Node js Tutorial/routes.js
+++ Node.js Tutorial Bangla  Express.js Tutorial Bangla Series for Beginners/#12 - Raw Node Project (Part 5) - Node.js Tutorial Bangla  Bangla Node js Tutorial/routes.js
@@ -11,12 +11,14 @@
 const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
 const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
+const { checkHandler } = require('./handlers/routeHandlers/checkHandler');
 
 // module scaffolding
 const routes = {
     sample: sampleHandler,
     user: userHandler,
     token: tokenHandler,
+    check: checkHandler,
 };
 
 
