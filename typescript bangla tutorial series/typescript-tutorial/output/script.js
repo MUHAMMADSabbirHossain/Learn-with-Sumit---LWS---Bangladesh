9f9dd5ef66ec9f515e79a681af3d52081e6f70a5
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-31 08:24:40 +0600

    complete: Lesson 2 - Editor Setup & Compilation - TypeScript Bangla ( বাংলা ) Tutorial Series

---
+++ typescript bangla tutorial series/typescript-tutorial/output/script.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+const country = "I love Bangladesh";
+console.log(country);
+//# sourceMappingURL=script.js.map
---
+++ typescript bangla tutorial series/typescript-tutorial/src/script.ts
@@ -0,0 +1,4 @@
+console.log("Hello World");
+
+const country = "I love Bangladesh";
+console.log(country);
