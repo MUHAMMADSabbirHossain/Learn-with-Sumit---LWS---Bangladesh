d0ac66d1665f725ada9a6cb0a4671b06ba873a29
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-31 08:53:19 +0600

    Complete: Lesson 3 - Type Basics - TypeScript Bangla ( বাংলা ) Tutorial Series

--- typescript bangla tutorial series/typescript-tutorial/src/script.ts
+++ typescript bangla tutorial series/typescript-tutorial/src/script.ts
@@ -1,4 +1,55 @@
-console.log("Hello World");
+/* console.log("Hello World");
 
 const country = "I love Bangladesh";
-console.log(country);
+console.log(country); */
+
+/* let playerName = "Mashrafi";
+let age = 35;
+
+console.log(playerName);
+
+playerName = 34;
+age = 34;
+age = "string";
+
+console.log(playerName); */
+
+/* let playerName;
+console.log(playerName);
+
+playerName = "Mashrafi";
+console.log(playerName);
+
+playerName = 34;
+console.log(playerName); */
+
+/* function multiply(a: number, b: number) {
+  return a * b;
+}
+console.log(multiply(5, 6));
+console.log(multiply("string", "6")); // should not do this */
+
+/* // array
+let fruits = ["apple", "banana", "orange", "mango", 200, true];
+console.log(fruits);
+
+fruits.push("pineapple");
+console.log(fruits);
+
+fruits.push({ name: "apple" }); // Argument of type '{ name: string; }' is not assignable to parameter of type 'string | number | boolean'.ts(2345)
+console.log(fruits); */
+
+/* // object
+let person = {
+  name: "Sumit",
+  age: 35,
+  isCaptain: true,
+  getName: function () {
+    return "Sumit";
+  },
+};
+
+person.name = "Sumit";
+person.age = "5"; // Type 'string' is not assignable to type 'number'.
+person.isCaptain = true;
+person.country = "Bangladesh"; // Property 'country' does not exist on type '{ name: string; age: number; isCaptain: boolean; getName: () => string; } */
