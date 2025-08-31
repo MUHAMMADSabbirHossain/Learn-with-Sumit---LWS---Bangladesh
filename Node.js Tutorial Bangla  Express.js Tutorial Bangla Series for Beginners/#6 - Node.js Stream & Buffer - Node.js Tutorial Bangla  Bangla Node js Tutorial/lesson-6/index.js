5724c12d37513eb923812e8aa1d6966f33583796
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-31 07:27:26 +0600

    fix: streamline read stream implementation in HTTP server

--- Node.js Tutorial Bangla  Express.js Tutorial Bangla Series for Beginners/#6 - Node.js Stream & Buffer - Node.js Tutorial Bangla  Bangla Node js Tutorial/lesson-6/index.js
+++ Node.js Tutorial Bangla  Express.js Tutorial Bangla Series for Beginners/#6 - Node.js Stream & Buffer - Node.js Tutorial Bangla  Bangla Node js Tutorial/lesson-6/index.js
@@ -201,8 +201,8 @@ ourReadStream.pipe(ourWriteStream);
 
 
 const server = http.createServer((req, res) => {
-    const myReadStream = fs.createReadStream(__dirname + '/bigdata.txt', 'utf-8');
-    myReadStream.pipe(res);
+  const myReadStream = fs.createReadStream(__dirname + '/bigdata.txt', 'utf-8');
+  myReadStream.pipe(res);
 });
 server.listen(3000);
 
