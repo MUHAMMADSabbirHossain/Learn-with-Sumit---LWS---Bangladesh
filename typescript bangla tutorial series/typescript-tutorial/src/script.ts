c118764de82dc62a2b52689765c0692af53581ad
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-31 13:45:30 +0600

    Complete: Lesson 7 - Type Aliases - TypeScript Bangla ( বাংলা ) Tutorial Series

--- typescript bangla tutorial series/typescript-tutorial/src/script.ts
+++ typescript bangla tutorial series/typescript-tutorial/src/script.ts
@@ -114,3 +114,14 @@ const towFunc: Function = (a: string, b: number, c?: string | number = 10) => {
 };
 towFunc("string", 34);
  */
+
+type stringOrNumber = string | number;
+type userType = { name: string; age: number };
+
+const userDetails = (id: stringOrNumber, user: userType) => {
+  console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+
+const sayHello = (user: userType) => {
+  console.log(`Hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
